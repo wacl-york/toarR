dbc7927249b74f54576124b240204ffd7a755088
willdrysdale <wsd500@york.ac.uk>
2023-10-26 14:01:00 +0100

    Add list_controlled_vocabulary
    `list_controlled_vocabulary()` returns the particulars of a type of controlled vocabulary

--- R/list_endpoints.R
+++ R/list_endpoints.R
@@ -13,6 +13,7 @@ the$endpointSearchArgs = NULL
 the$endpointDataDesc = NULL
 the$endpointVariablesDesc = NULL
 the$endpointControlledVocabularyDesc = NULL
+the$controlledVocabulary = list()
 
 #' List Endpoints
 #'
@@ -114,9 +115,9 @@ list_endpoint_args = function(endpoint, force = FALSE){
         stn = list_endpoint_args("stationmeta")
         ts = list_endpoint_args("timeseries")
 
-        endpointArgs = bind_rows(stn,ts) |>
+        endpointArgs = dplyr::bind_rows(stn,ts) |>
           dplyr::distinct() |>
-          dplyr::filter(Name != "id") |>
+          dplyr::filter(name != "id") |>
           janitor::clean_names()
         # id does not apply to the search endpoint as it is a cross reference of # stationmeta and timeseries
 
@@ -148,5 +149,56 @@ list_endpoint_args = function(endpoint, force = FALSE){
     return(the[[envEndpointName]])
 
   }
+
+}
+
+#' List Controlled Controlled Vocabulary
+#'
+#' Provides the data table associated with the defined controlled Vocabulary.
+#' A description of the controlled vocabulary can be generated with \code{list_endpoint_args("controlled_vocabulary")}
+#' The response will be stored in the internal environment, which can be overridden with \code{force = TRUE}.
+#'
+#' @param controlled_vocabulary the name of the controlled vocabulary required.
+#' Note these are formatted with begining with each word starting with a capital and
+#' separated with a space. Arguments will be formatted as such using \code{stringr::str_to_title()}.
+#' They will then be formatted for the url via \code{utils:URLencode()}. string.
+#'
+#' @param force force the function to re-scrape the API docs. boolean.
+#'
+#' @export
+#'
+
+list_controlled_vocabulary = function(controlledVocabulary, force = FALSE){
+
+  # This is precarious.
+  validControlledVocabulary = list_endpoint_args("controlled_vocabulary")
+  CVStart = which(stringr::str_detect(validControlledVocabulary$description, "Valid names"))+1
+  CVEnd = which(stringr::str_detect(validControlledVocabulary$description, "Example"))-1
+  validControlledVocabulary = validControlledVocabulary$description[CVStart:CVEnd]
+
+  controlled_vocabulary = stringr::str_to_title(controlledVocabulary)
+
+  if(!controlledVocabulary %in% validControlledVocabulary){
+    stop(paste0("controlled_vocabulary must be one of: ",
+                paste(validControlledVocabulary, collapse = ", ")))
+  }
+
+  if(is.null(the$controlledVocabulary[[controlledVocabulary]]) | force){
+    response = paste0(get_endpoint_url("controlled_vocabulary"),controlledVocabulary) |>
+      utils::URLencode() |>
+      query_toar_database() |>
+      purrr::pluck(1) |>
+      as.data.frame() |>
+      dplyr::tibble()
+
+    the$controlledVocabulary[[controlledVocabulary]] = response
+
+    #
+    return(response)
+  }else{
+    return(the$controlledVocabulary[[controlledVocabulary]])
+  }
+
+
 
 }
