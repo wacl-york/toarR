913ed4995eaed423c9bb6b0331a9e0b268859bda
willdrysdale <wsd500@york.ac.uk>
2023-11-03 10:40:33 +0000

    fix the formating for data_start_date and data_end_date
    Turns out submitting it with `+00:00` (or other timezones) causes the API to error.
    
    Note as part of this I think I discovered that the API very much interprets these arguments as data_start_date == the argument, not data_start_date is <= the argument.

--- R/utils.R
+++ R/utils.R
@@ -1,26 +1,8 @@
 formatDate = function(date){
 
-  arg = deparse(substitute(date))
-
   date = date |>
     as.character() |>
     stringr::str_replace(" ","T")
-
-  # there is 100% a better way to do this
-  # but for now this works-ish
-  if(nchar(date) == 10){
-    date = paste0(date,"T00:00:00")
-  }
-
-  if(nchar(date) == 13){
-    date = paste0(date,":00:00")
-  }
-
-  if(nchar(date) == 16){
-    date = paste0(date,":00")
-  }
-
-  date = paste0(date,"+00:00")
 
   #
   date
